a99598ddc96384a56d27e32812aff45b445d7604
travism26 <travismartin266@gmail.com>
2017-05-04 18:37:39 -0300

    add components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+
+import Title from "./Header/Title"
 class Header extends Component {
 
   render() {
     return (
-      <header>header</header>
+      <Title />
     );
   }
 }
