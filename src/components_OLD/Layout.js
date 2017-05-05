05046c3c3d912256301e1d6b16abfc027854616c
travism26 <travismartin266@gmail.com>
2017-05-04 22:03:35 -0300

    trying something new

---
+++ src/components_OLD/Layout.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './../logo.svg';
+import '../App.css';
+
+import Footer from "./Footer";
+import Header from "./Header";
+
+class Layout extends Component {
+  render() {
+    const title = "Welcome Travis!";
+    return (
+      <div className="App">
+        <h1>hello world</h1>
+        
+      </div>
+    );
+  }
+}
+
+export default Layout;
---
+++ src/pages/Layout.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './../logo.svg';
+import '../App.css';
+
+class Layout extends Component {
+  render() {
+    const title = "Welcome Travis!";
+    return (
+      <div className="App">
+        <h1>hello world</h1>
+      </div>
+    );
+  }
+}
+
+export default Layout;
